fe170fd97fc77bab64e2fcc29ff12240da55e978
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-10-18 23:08:06 +0300

    To-do-list

---
+++ modules/index.js
@@ -0,0 +1,88 @@
+import {
+    getProductsFromLocalStorage,
+    addProductToLocalStorage,
+    updateProductToLocalStorage,
+    deleteProductFromLocalStorage,
+} from "./local-storage.js"
+
+
+
+const todoList = document.querySelector(".todo-list")
+const form = document.querySelector("form")
+const Input = document.querySelector("input")
+const Create = document.querySelector(".create")
+const All = document.querySelector(".all")
+const todo = document.querySelector(".todo")
+const completed = document.querySelector(".completed")
+const ul = document.querySelector("ul")
+const line = document.querySelector(".active")
+
+
+const inputValue = Input.value
+
+
+
+
+
+
+let tasks = getProductsFromLocalStorage()
+if (getProductsFromLocalStorage()) {
+    tasks.map(task => {
+        addTask(task)
+    })
+}
+
+form.addEventListener("click", (e) => {
+    e.preventDefault()
+    const inputValue = Input.value
+    if (inputValue === '') {
+        return
+    }
+    const today = new Date()
+    const result = today.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+    });
+    const task = {
+        id: Date.now(),
+        title: inputValue,
+        completed: false,
+        createdAt: `${result}.${today.getDate()}/${today.getMonth() + 1}/${today.getFullYear()}`,
+
+    }
+    tasks.push(task)
+    addProductToLocalStorage(task)
+    addTask(task)
+})
+
+function addTask(task) {
+    let li = document.createElement("li")
+    li.setAttribute('id', task.id)
+
+    li.innerHTML = task.title
+    li.setAttribute('class', 'task')
+    let span = document.createElement("span")
+    span.setAttribute('class', 'check')
+    if(task.completed){
+        span.className = "check circle"
+    }
+    li.appendChild(span)
+    let div = document.createElement("div")
+    div.innerHTML = `<span>${task.createdAt}</span>`
+    li.appendChild(div)
+    ul.appendChild(li)
+    Input.value = ""
+}
+ul.addEventListener("click" , (e) => {
+    const hasClass = e.target.classList.contains("check")
+        if(hasClass){
+            const update = e.target.parentElement.getAttribute("id")
+            updateProductToLocalStorage(update , {completed : true})
+            e.target.classList.toggle('circle')
+            
+        }
+        
+})
+
---
+++ modules/local-storage.js
@@ -0,0 +1,44 @@
+const getProductsFromLocalStorage = () => {
+    const storedProducts = localStorage.getItem("products")
+    if (storedProducts) {
+        return JSON.parse(storedProducts)
+    }
+
+    return []
+}
+
+const addProductToLocalStorage = (product) => {
+    const currentProducts = getProductsFromLocalStorage()
+    const uniqueId = Date.now()
+    currentProducts.push({...product, id: uniqueId})
+    setProductsToLocalStorage(currentProducts)
+}
+
+const updateProductToLocalStorage = (id, updatedDetails) => {
+    const currentProducts = getProductsFromLocalStorage()   
+    for (let i = 0; i < currentProducts.length; i++) {
+        if (currentProducts[i].id === id) {
+             currentProducts[i] = {...currentProducts[i], ...updatedDetails}
+            break 
+/*             currentProducts[i].completed == false ? (currentProducts[i].completed = true) : (currentProducts[i].completed)
+ */        }
+    }
+    setProductsToLocalStorage(currentProducts)
+}
+
+const deleteProductFromLocalStorage = (id) => {
+    const currentProducts = getProductsFromLocalStorage()
+    const productsAfterDeletion = currentProducts.filter(product => product.id !== id)
+    setProductsToLocalStorage(productsAfterDeletion)
+}
+
+const setProductsToLocalStorage = (products) => {
+    localStorage.setItem("products", JSON.stringify(products))
+}
+
+export {
+    getProductsFromLocalStorage,
+    addProductToLocalStorage,
+    updateProductToLocalStorage,
+    deleteProductFromLocalStorage,
+}
